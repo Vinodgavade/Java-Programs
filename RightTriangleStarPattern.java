f6c6737cd9f9eb5741767d691233bc3de628919d
Vinodgavade <114814764+Vinodgavade@users.noreply.github.com>
2023-12-02 12:42:41 +0530

    Add files via upload

---
+++ RightTriangleStarPattern.java
@@ -0,0 +1,23 @@
+
+public class RightTriangleStarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i, j, row=6;   
+		
+		for(i=0; i<row; i++)   
+		{   
+		 
+		for(j=0; j<=i ; j++)   
+		{   
+		//prints stars  
+		System.out.print("* ");   
+		}   
+		
+		System.out.println();   
+		}   
+		
+	}
+
+}
